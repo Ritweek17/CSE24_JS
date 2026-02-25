8e96be6cffc96094ce14b466449c1e0ac4af2ad7
Ritweek Dubey <ritweekdubey2006@gmail.com>
2026-02-25 19:32:04 +0530

    operators

---
+++ Arithmetic.js
@@ -0,0 +1,40 @@
+var a = 100 ;
+var b = 50 ;
+
+document.write("a = " + a + "<br/>") ;
+document.write("<br>") ;
+document.write("b = " + b + "<br/>") ;
+document.write("<br>") ;
+document.write("<br>") ;
+
+
+document.write("a + b = " ) ;
+result = a + b ;
+document.write(result) ;
+document.write("<br>") ;
+document.write("<br>") ;
+
+
+document.write("a - b = " ) ;
+result = a - b ;
+document.write(result) ;
+document.write("<br>") ;
+document.write("<br>") ;
+
+document.write("a * b = " ) ;
+result = a * b ;
+document.write(result) ;
+document.write("<br>") ;
+document.write("<br>") ;
+
+document.write("a / b = " ) ;
+result = a / b ;
+document.write(result) ;
+document.write("<br>") ;
+document.write("<br>") ;
+
+document.write("a % b = " ) ;
+result = a % b ;
+document.write(result) ;
+document.write("<br>") ;
+document.write("<br>") ;
---
+++ comparison.js
@@ -0,0 +1,39 @@
+var a = 100 ;
+var b = 50 ;
+var linebreak = "<br>";
+
+document.write("a = " + a + linebreak);
+document.write("<br>") ;
+document.write("b = " + b + linebreak);
+document.write("<br>") ;
+document.write("<br>") ;
+
+document.write("a > b is " );
+result = a > b ;
+document.write(result + linebreak);
+document.write(linebreak);
+
+document.write("a < b is " );
+result = a < b ;
+document.write(result + linebreak);
+document.write(linebreak);
+
+document.write("a >= b is " );
+result = a >= b ;
+document.write(result + linebreak);
+document.write(linebreak);
+
+document.write("a <= b is " );
+result = a <= b ;
+document.write(result + linebreak);
+document.write(linebreak);
+
+document.write("a == b is " );
+result = a == b ;
+document.write(result + linebreak);
+document.write(linebreak);
+
+document.write("a != b is " );
+result = a != b ;
+document.write(result + linebreak);
+document.write(linebreak);
