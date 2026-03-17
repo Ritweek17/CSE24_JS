8774ababc69da9f1e687a1fb491de2b533e60123
Ritweek Dubey <ritweekdubey2006@gmail.com>
2026-03-17 15:31:12 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var myInfo = new Array();
+var myInfo1 = Array();
+document.write(myInfo==myInfo1 + "<br>"); 
+
+let collection = ["ritweek" , "shreya" , "satyarth"];
+document.write(collection[0] + "<br>");
+document.write(collection[1] + "<br>");
+document.write(collection[2] + "<br>");
+document.write(collection.length + "<br>");
+document.write(collection[collection.length-1] + "<br>");
+
+collection[1] = "shreya gupta";
+document.write(collection[1] + "<br>");
+
+
