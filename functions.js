79275d383c234f8a8dc32bf7a9032fdcf6ec3aa4
Ritweek Dubey <ritweekdubey2006@gmail.com>
2026-03-17 15:15:37 +0530

    functions

--- functions.js
+++ functions.js
@@ -1,3 +1,20 @@
 function myfunction() {
     document.write("Hello World");
 }
+function sayHello(){
+    let nam = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+    document.write("Hello " + nam + ", you are " + age + " years old.");
+}
+let x = 7 ;
+let y = 8;
+let add = (x,y) => {
+    return x+y ;
+}
+document.write (add (x ,y));
+
+
+
+    
+
+
