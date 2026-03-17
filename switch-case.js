031ad848755f6a4ac4d7298edb8f3a7c2a9be649
Ritweek Dubey <ritweekdubey2006@gmail.com>
2026-03-17 15:00:03 +0530

    switch case

---
+++ switch-case.js
@@ -0,0 +1,15 @@
+var grade = 'A';
+
+switch(grade) {
+    case 'A':
+        document.write("Excellent!");
+        break;
+    case 'B':
+        document.write("Good!");
+        break;
+    case 'C':
+        document.write("Average!");
+        break;
+    default:
+        document.write("Invalid grade.");
+}
