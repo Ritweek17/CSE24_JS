8519ebb17d9e0b439ca6182d54c3cb3effc80fc8
Ritweek Dubey <ritweekdubey2006@gmail.com>
2026-03-17 15:25:05 +0530

    object

---
+++ object.js
@@ -0,0 +1,19 @@
+// object 
+var person = {
+    name : john Mac , 
+    age : 30 ,
+    isMarried : true ,
+    address : {
+        street : 'vijay nagar' ,
+        house : 5 ,
+    }
+}
+var person2 = new Object();
+person2.name = "krishna";
+person2.age = 32;
+person2.isMarried = false ;
+person2.address = {};
+person2.address.street = "viyaj nagar"
+person2['address']['flat no']= 33 ;
+
+
